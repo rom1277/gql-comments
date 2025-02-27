1929b19baa92e74fdfbb10422d0811e1adf41aed
Rom <rom@RomUbuntu>
2025-02-27 19:03:22 +0300

    delete comment

--- graph/resolver.go
+++ graph/resolver.go
@@ -40,27 +40,6 @@ func (r *mutationResolver) CreatePost(ctx context.Context, title string, content
 	return &post, nil
 }
 
-// type mutationResolver struct{ *Resolver }
-
-// func (r *mutationResolver) CreatePost(ctx context.Context, title string, content string, allowComments bool) (*storage.Post, error) {
-// 	if title == "" || content == "" {
-// 		return nil, errors.New("title and content must not be empty")
-// 	}
-
-// 	id := fmt.Sprintf("post-%d", rand.Intn(1000000))
-// 	post := storage.Post{
-// 		ID:            id,
-// 		Title:         title,
-// 		Content:       content,
-// 		AllowComments: allowComments,
-// 	}
-
-// 	r.Storage.CreatePost(post)
-// 	return &post, nil
-// }
-
-// type queryResolver struct{ *Resolver }
-
 func (r *queryResolver) Posts(ctx context.Context) ([]*storage.Post, error) {
 	posts := r.Storage.GetAllPosts()
 	var result []*storage.Post
