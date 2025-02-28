293e064a05b9e2ac631178466f3d5f611d02032b
Rom <rom@RomUbuntu>
2025-02-28 19:27:45 +0300

    delete comments

--- graph/resolvers/resolver.go
+++ graph/resolvers/resolver.go
@@ -1,9 +1,5 @@
 package resolvers
 
-// This file will not be regenerated automatically.
-
-// It serves as dependency injection for your app, add any dependencies you require here.
-
 import (
 	"context"
 	"errors"
